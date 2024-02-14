e16ba44f7c32e63b5fb5302392f2188bd00666f7
Ankit Kumar Pradhan <aankitkumarpradhan@gmail.com>
2024-02-15 03:18:00 +0530

    new changes

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,3 @@
-const Page = () => {
-    return (
-        <p>Invoices Page</p>
-    )
-}
-Page.displayName = "Page";
-export default Page;
+export default function Page() {
+    return <p>Invoices Page</p>;
+  }
